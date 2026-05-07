026c476479f5fe40cd84ce0cdd8b3c8d866a0a1a
Isaac Mills <rooster0055@protonmail.com>
2026-05-07 14:20:19 -0600

    Make health check endpoint be in package

--- spring_packages/lockinspiel-analyzer/src/main/java/live/lockinspiel/analyzer/AnalyzerController.java
+++ spring_packages/lockinspiel-analyzer/src/main/java/live/lockinspiel/analyzer/AnalyzerController.java
@@ -1,9 +1,11 @@
-import org.springframework.stereotype.Controller;
+package live.lockinspiel.analyzer;
+
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@RestController
 public class AnalyzerController {
 
 	@GetMapping("/")
