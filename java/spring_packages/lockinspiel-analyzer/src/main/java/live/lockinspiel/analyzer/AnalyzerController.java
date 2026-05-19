4b4a92a693137d436ad39651aaa40367943b1a13
Makaden Espinosa <mespinosa@student.neumont.edu>
2026-05-19 08:26:07 -0600

    Analizer base set up

--- java/spring_packages/lockinspiel-analyzer/src/main/java/live/lockinspiel/analyzer/AnalyzerController.java
+++ java/spring_packages/lockinspiel-analyzer/src/main/java/live/lockinspiel/analyzer/AnalyzerController.java
@@ -8,9 +8,57 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class AnalyzerController {
 
+	/***
+	 * Used for health checks to ensure that the service is up and running.
+	 * Typically will be used by Kubernetes to verify the state of this service.
+	 * 
+	 * @return A string indicating the health status of the service.
+	 */
 	@GetMapping("/")
 	public String HealthCheck() {
 		return "up";
+	}
+
+	/***
+	 * Will return the total time spent working with the Pomodoro Timer running.
+	 * This can be filtered by tags to isolate the time spent working on specific
+	 * categories.
+	 * Examples of "tags" could be: personal projects, hobbies, sports, etc.
+	 * 
+	 * @param tags
+	 * @return Will return a string of the complete time spent working with the
+	 *         Pomodoro Timer running. This may change form a string to a time based
+	 *         object depending on how we want to represent the data.
+	 */
+	@GetMapping("/analyzer/worktime")
+	public String getWorktime(@RequestParam(required = false) String tags) {
+		if (tags != null) {
+			// This is where we filter by tags.
+		}
+		return "Implement functionality";
+	}
+
+	@GetMapping("/analyzer/breaktime")
+	public String getBreaktime() {
+
+		return "Implement functionality";
+	}
+
+	/***
+	 * Will return information about the ratio spent working vs taking breaks.
+	 * Will be able to be filtered by school, work, and other as well to isolate the
+	 * ratio for each of those categories.
+	 * 
+	 * @return A string representing the ratio. It is a string for now, but may be
+	 *         subject to change depending on how we want to represent the data. It
+	 *         could be a percentage, a ratio, or some other format.
+	 * 
+	 */
+
+	@GetMapping("/analyzer/ratio")
+	public String getRatio() {
+
+		return "Implement functionality";
 	}
 
 }
