c830ea1eb772075623b58b25d22dd30d80fb3f67
Target-null <Rollin.m.place@icloud.com>
2026-05-19 11:06:13 -0600

    User Profile and Saved list Mocked up

--- bun/packages/lockinspiel-frontend/src/app/main-page/main-page.ts
+++ bun/packages/lockinspiel-frontend/src/app/main-page/main-page.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Timer } from '../timer/timer';
 import { SetTimer } from '../set-timer/set-timer';
-import { UserProfile } from '../user-profile/user-profile';
+import { UserProfile, UserData } from '../user-profile/user-profile';
 import { SavedList } from '../saved-list/saved-list';
 import { FriendList } from '../friend-list/friend-list';
 import { Setting } from '../setting/setting';
@@ -20,12 +20,39 @@ export class MainPage {
   workMinutes = signal(25);
   restMinutes = signal(5);
 
+  modalOpen = signal(false);
+  modalUser = signal<UserData>({
+    username: 'Username',
+    bio: 'Bio',
+    status: 'offline',
+    isOwn: true,
+  });
+
   toggleFriends() {
     this.friendsOpen.set(!this.friendsOpen());
   }
 
   toggleSaved() {
     this.savedOpen.set(!this.savedOpen());
+  }
+
+  openOwnProfile() {
+    this.modalUser.set({
+      username: 'Rollin',
+      bio: 'CEO of Frontend',
+      status: 'free',
+      isOwn: true,
+    });
+    this.modalOpen.set(true);
+  }
+
+  openFriendProfile(user: UserData) {
+    this.modalOpen.set(true);
+    this.modalUser.set({ ...user, isOwn: false });
+  }
+
+  closeModal() {
+    this.modalOpen.set(false);
   }
 
   onTimerStarted(config: { workMinutes: number; restMinutes: number }) {
