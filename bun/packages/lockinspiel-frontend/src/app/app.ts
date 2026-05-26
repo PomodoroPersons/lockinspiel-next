e4da93bde28de40e16fba0af9532332eb0836666
Isaac Mills <rooster0055@protonmail.com>
2026-05-26 11:10:50 -0600

    Add persistent auth via refresh token

--- bun/packages/lockinspiel-frontend/src/app/app.ts
+++ bun/packages/lockinspiel-frontend/src/app/app.ts
@@ -1,5 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, Inject, inject, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { client } from '../api-client/client.gen';
+import { SessionService } from '../api-client';
+import { HttpClient } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('lockinspiel-frontend');
+
+  #sessionService = inject(SessionService);
+  #http = inject(HttpClient);
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  async ngOnInit() {
+    if (this.isBrowser) {
+      let { data: accessToken } = await this.#sessionService.authNewSession({
+        httpClient: this.#http,
+        body: {
+          refresh_token: {}
+        }
+      });
+
+      client.setConfig({
+        auth: accessToken?.access_token
+      });
+    }
+  }
 }
